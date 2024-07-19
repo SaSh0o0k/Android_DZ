c382f962fa2f021bd53e9b54cd28efb7faba956c
Oleksandr Putsyk <sashaputsyk100903@gmail.com>
2024-07-19 15:10:22 +0300

    Add Login and Register

--- Shop/app/src/main/java/com/example/shop/network/RetrofitClient.java
+++ Shop/app/src/main/java/com/example/shop/network/RetrofitClient.java
@@ -1,7 +1,12 @@
 package com.example.shop.network;
 
 import com.example.shop.config.Config;
+import com.example.shop.interceptors.JWTInterceptor;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,9 +15,20 @@ public class RetrofitClient {
     private Retrofit retrofit;
 
     public RetrofitClient() {
+        HttpLoggingInterceptor interceptor =  new  HttpLoggingInterceptor ();
+        interceptor.setLevel( HttpLoggingInterceptor.Level.BODY );
+
+        OkHttpClient.Builder client =  new  OkHttpClient.Builder()
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .addInterceptor(new JWTInterceptor())
+                .addInterceptor(interceptor);
+
         retrofit = new Retrofit.Builder()
                 .baseUrl(Config.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client.build())
                 .build();
     }
 
@@ -22,13 +38,14 @@ public class RetrofitClient {
         }
         return instance;
     }
-
     public CategoriesApi getCategoriesApi() {
         return retrofit.create(CategoriesApi.class);
     }
     public PizzaApi getPizzaApi() {
         return retrofit.create(PizzaApi.class);
     }
+    public AccountApi getAccountApi() { return retrofit.create(AccountApi.class); }
+
 
 
 }
